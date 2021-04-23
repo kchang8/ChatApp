065c6d9efbb0eec779300a83cff5abdf2c0985e3
joey <joeyortiz2016@gmail.com>
2021-04-22 18:18:46 -0700

    included sign out text view for home activity

--- app/src/main/java/com/bignerdranch/android/chatapp2/HomeActivity.kt
+++ app/src/main/java/com/bignerdranch/android/chatapp2/HomeActivity.kt
@@ -1,12 +1,17 @@
 package com.bignerdranch.android.chatapp2
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.bignerdranch.android.chatapp2.fragments.ChatFragment
 import com.bignerdranch.android.chatapp2.fragments.SearchFragment
 import com.bignerdranch.android.chatapp2.fragments.SettingsFragment
 import com.bignerdranch.android.chatapp2.fragments.adapters.ViewPagerAdapter
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -15,7 +20,28 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        verifyUserIsLoggedIn()
         setUpTabs()
+
+        val signOut: TextView = findViewById(R.id.home_sign_out)
+        signOut.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, RegisterActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+
+
+    }
+
+
+    private fun verifyUserIsLoggedIn() {
+        val uid = FirebaseAuth.getInstance().uid
+        if (uid == null) {
+            val intent = Intent(this, RegisterActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
     }
 
     private fun setUpTabs() {
@@ -30,4 +56,17 @@ class HomeActivity : AppCompatActivity() {
         tabs.getTabAt(1)!!.setIcon(R.drawable.ic_baseline_search_24)
         tabs.getTabAt(2)!!.setIcon(R.drawable.ic_baseline_settings_24)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, RegisterActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
