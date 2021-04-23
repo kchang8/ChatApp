482e9605bf22a22714f97155d1494b3c6478d678
Dianne Lopez <diannel@csu.fullerton.edu>
2021-04-22 18:08:16 -0700

    Display Username
    pls halp with profile pic

--- app/src/main/java/com/bignerdranch/android/chatapp2/HomeActivity.kt
+++ app/src/main/java/com/bignerdranch/android/chatapp2/HomeActivity.kt
@@ -1,19 +1,51 @@
 package com.bignerdranch.android.chatapp2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.bignerdranch.android.chatapp2.fragments.ChatFragment
 import com.bignerdranch.android.chatapp2.fragments.SearchFragment
 import com.bignerdranch.android.chatapp2.fragments.SettingsFragment
 import com.bignerdranch.android.chatapp2.fragments.adapters.ViewPagerAdapter
+import com.bignerdranch.android.chatapp2.modelClasses.Users
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
+    var refUsers: DatabaseReference? = null
+    var firebaseUser: FirebaseUser? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //Display username and provide pic
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        refUsers = FirebaseDatabase.getInstance().reference.child("users").child(firebaseUser!!.uid)
+
+        refUsers!!.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot){
+                if (p0.exists()){
+
+                    val user: Users? = p0.getValue(Users::class.java)
+
+                    home_username.text = user!!.getUsername()
+                    // HALP
+                    Picasso.get().load(user.getImageURL()).into(home_profilePicture)
+
+                }
+
+            }
+
+            override fun onCancelled(p0: DatabaseError){
+
+            }
+        })
 
         setUpTabs()
     }
