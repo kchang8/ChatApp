f8b44251a384e1aa780be2e2bce4e2af5cf7296e
kchang8 <kailiec@csu.fullerton.edu>
2021-04-23 17:54:37 -0700

    added add post page in feed fragment, successfully saves post to db

--- app/src/main/java/com/bignerdranch/android/chatapp2/fragments/FeedFragment.kt
+++ app/src/main/java/com/bignerdranch/android/chatapp2/fragments/FeedFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.chatapp2.AddPostActivity
 import com.bignerdranch.android.chatapp2.HomeActivity
 import com.bignerdranch.android.chatapp2.R
@@ -27,10 +29,18 @@ class FeedFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_feed, container, false)
 
+        var recyclerView: RecyclerView? = null
+
         view.feed_addPostFab.setOnClickListener {
             val intent = Intent(activity, AddPostActivity::class.java)
             startActivity(intent)
         }
+
+        recyclerView = view.findViewById(R.id.feed_recyclerView)
+        val linearLayoutManager = LinearLayoutManager(context)
+        linearLayoutManager.reverseLayout = true
+        linearLayoutManager.stackFromEnd = true
+        recyclerView.layoutManager = linearLayoutManager
 
         return view
     }
