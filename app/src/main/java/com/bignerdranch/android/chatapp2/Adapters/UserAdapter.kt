066d9a672afb56fa758a63904118681ac33c885c
joey <joeyortiz2016@gmail.com>
2021-04-29 16:44:09 -0700

    polished up friend request system

--- app/src/main/java/com/bignerdranch/android/chatapp2/Adapters/UserAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/chatapp2/Adapters/UserAdapter.kt
@@ -7,13 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.chatapp2.R
 import com.bignerdranch.android.chatapp2.modelClasses.Users
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.user_item_layout.view.*
@@ -40,16 +44,30 @@ class UserAdapter
         holder.searchUsernameTextView.text = user.getUsername()
 
         holder.searchAddButton.setOnClickListener(){
-            Log.d("addingFriend", "now adding friend")
             val db = FirebaseDatabase.getInstance()
             val auth = FirebaseAuth.getInstance()
             var addUser = holder.searchUsernameTextView.text.toString()
+
+            //this for loop is bad code yiiiikes
             for (user in mUser){
                 if (user.getUsername() == addUser){
                     addUser = user.getUID()!!
                 }
             }
-            val setUser = db.reference.child("users").child(auth.uid.toString()).child("friendList").child(addUser).setValue(false)
+
+            //gets node at which the addUser should be at
+            val friendList = db.reference.child("users").child(addUser).child("friendList").child(auth.uid.toString()).get()
+            friendList.addOnSuccessListener {
+                if (it.exists()){
+                    Log.d("friendAlreadyAdded", "You already made a friend request ")
+                }
+                //writes to targeted user's information
+                else{
+                    Log.d("addingFriend", "now adding friend")
+                    db.reference.child("users").child(addUser.toString()).child("friendList").child(auth.uid.toString()).setValue(false)
+                }
+            }.addOnFailureListener {
+            }
         }
 
         //Picasso.get().load(user.getImageURL()).placeholder(R.drawable.ic_baseline_person_24).into(holder.searchProfileImage)
