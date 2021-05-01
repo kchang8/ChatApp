02320f8a297886ae14803ba101092d952e35da90
kchang8 <kailiec@csu.fullerton.edu>
2021-04-30 18:36:03 -0700

    added more activities to chat fragment

---
+++ app/src/main/java/com/bignerdranch/android/chatapp2/NewMessageActivity.kt
@@ -0,0 +1,107 @@
+package com.bignerdranch.android.chatapp2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bignerdranch.android.chatapp2.modelClasses.Users
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_new_message.*
+import kotlinx.android.synthetic.main.new_message_user_item.view.*
+
+class NewMessageActivity : AppCompatActivity() {
+
+    lateinit var db: FirebaseDatabase
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_message)
+
+        db = FirebaseDatabase.getInstance()
+
+//        val adapter = GroupAdapter<ViewHolder>()
+//
+//        adapter.add(UserItem())
+//        adapter.add(UserItem())
+//        adapter.add(UserItem())
+//
+//        newMessage_recyclerView.adapter = adapter
+        newMessage_recyclerView.layoutManager = LinearLayoutManager(this)
+
+        fetchUsers()
+
+        newMessage_backButton.setOnClickListener {
+            finish()
+        }
+
+    }
+
+    private fun fetchUsers() {
+        val ref = db.getReference("/users")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                val adapter = GroupAdapter<ViewHolder>()
+
+                p0.children.forEach {
+                    Log.d("NewMessage", it.toString())
+                    val user = it.getValue(Users::class.java)
+                    if (user != null) {
+                        adapter.add(UserItem(user))
+                    }
+                }
+
+                adapter.setOnItemClickListener { item, view ->
+                    val intent = Intent(view.context, ChatLogActivity::class.java)
+                    startActivity(intent)
+                }
+
+                newMessage_recyclerView.adapter = adapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+}
+
+class UserItem(val user: Users): Item<ViewHolder>() {
+    private var refUsers: DatabaseReference? = null
+
+    lateinit var db: FirebaseDatabase
+
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        db = FirebaseDatabase.getInstance()
+
+        // get user's usernames to display
+        viewHolder.itemView.newMessage_username.text = user.getUsername()
+
+        // get user's profile pics to display
+        refUsers = db.reference.child("users").child(user.getUID()!!)
+        refUsers!!.addValueEventListener(object : ValueEventListener
+        {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists())
+                {
+                    val imageUrl = snapshot.child("profileImageUrl").getValue().toString()
+
+                    Picasso.get().load(imageUrl).into(viewHolder.itemView.newMessage_profileImage)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.new_message_user_item
+    }
+}
+
--- app/src/main/java/com/bignerdranch/android/chatapp2/fragments/ChatFragment.kt
+++ app/src/main/java/com/bignerdranch/android/chatapp2/fragments/ChatFragment.kt
@@ -1,11 +1,14 @@
 package com.bignerdranch.android.chatapp2.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bignerdranch.android.chatapp2.NewMessageActivity
 import com.bignerdranch.android.chatapp2.R
+import kotlinx.android.synthetic.main.fragment_chat.view.*
 
 class ChatFragment : Fragment() {
 
@@ -19,7 +22,14 @@ class ChatFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        val view = inflater.inflate(R.layout.fragment_chat, container, false)
+
+        view.chat_newMessageImageView.setOnClickListener {
+            val intent = Intent(activity, NewMessageActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 
 }
